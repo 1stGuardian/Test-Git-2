cee0364c0f321f465df07d3f987d92cba8f77974
1stGuardian <ranmarurega@gmail.com>
2022-07-13 15:10:26 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 <body>
     <h1>HELLO WORLD</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cumque quis maiores officiis consequatur fugiat id iusto dignissimos, dolore, inventore consequuntur eveniet debitis amet! Perferendis tempora quia accusamus assumenda sequi.</p>
+    <ul>
+        <li>1</li>
+    </ul>
 </body>
 
 </html>
