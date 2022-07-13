95e7196753ee6caa622fcd864aa00a568133a2de
1stGuardian <ranmarurega@gmail.com>
2022-07-13 15:00:20 +0700

    Menambahkan paragraf

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
 
 <body>
     <h1>HELLO WORLD</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit cumque quis maiores officiis consequatur fugiat id iusto dignissimos, dolore, inventore consequuntur eveniet debitis amet! Perferendis tempora quia accusamus assumenda sequi.</p>
 </body>
 
 </html>
